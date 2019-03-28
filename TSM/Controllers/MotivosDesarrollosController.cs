3d5a98ecd75debeb4a3c195c76f4cd284bb65132
William Lopez <wlopez@techno-screen.com>
2019-03-28 23:54:23 +0000

    se incluye mtto para la definición de los tipos de maquinas, Tipos de luces , tipos de acabados, tipos de muestras, motivos de desarrollos Imported from Changeset 702

---
+++ TSM/Controllers/MotivosDesarrollosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSM.Controllers
+{
+    public class MotivosDesarrollosController : Controller
+    {
+        // GET: MotivosDesarrollos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
