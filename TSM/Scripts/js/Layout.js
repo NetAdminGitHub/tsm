3ebdbdc18cbc13357fd501e6c3eae69deb29f557
William Lopez <wlopez@techno-screen.com>
2019-05-28 16:08:02 +0000

    se excluye linea de codigo de genercion de Cookies "UserStateGrid" Imported from Changeset 781

--- TSM/Scripts/js/Layout.js
+++ TSM/Scripts/js/Layout.js
@@ -7,9 +7,7 @@ $(document).ready(function () {
     if (Cookies.get("user") != undefined)
         window.sessionStorage.setItem("user", Cookies.get("user"));
 
-    if (Cookies.get("UserStateGrid") === undefined)
-        Cookies.set("UserStateGrid", "[]");
- 
+
     vtoken();
 
     // oerde de ejecucion de documentos
