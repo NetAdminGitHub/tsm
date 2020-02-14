de28e8b0c6a97ec08524ea77019a4902f557a4cf
Juan Menjivar <jmenjivar@inquisalva.com>
2020-02-14 17:39:51 -0600

    Refactor configuracion PBi

---
+++ TSM/Controllers/PbiTokenController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections.Specialized;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
+using Microsoft.Rest;
+using TSM.Utils;
+
+
+namespace TSM.Controllers
+{
+    public class PbiTokenController : Controller
+    {
+
+ 
+
+       [HttpGet]
+        public void Validar()
+        {
+            //Redirect uri must match the redirect_uri used when requesting Authorization code.
+            string redirectUri = PbiUtils.PbiReport.RedirectUrl;//$"https://localhost:44311/PbiToken/Validar";
+            string authorityUri =PbiUtils.PbiReport.AADAuthorityUri;// "https://login.windows.net/common/";
+
+            // Get the auth code
+            string code = Request.Params["code"];
+
+            if (code != null)
+            {
+                // Get auth token from auth code       
+                TokenCache TC = new TokenCache();
+
+                AuthenticationContext AC = new AuthenticationContext(authorityUri, TC);
+                ClientCredential cc = new ClientCredential
+                    (PbiUtils.PbiReport.ApplicationId,
+                     PbiUtils.PbiReport.AppSecret);
+
+                AuthenticationResult AR = AC.AcquireTokenByAuthorizationCodeAsync(code, new Uri(redirectUri), cc).Result;
+
+                //Set Session "authResult" index string to the AuthenticationResult
+                Session[PbiUtils.authResultString] = AR;
+
+                //Get the authentication result from the session
+                PbiUtils.authResult = (AuthenticationResult)Session[PbiUtils.authResultString];
+
+                //Response.Redirect($"/{PbiUtils.EmbedType}.aspx");
+            }
+            else
+            {
+                //Remove Session "authResult"
+                Session[PbiUtils.authResultString] = null;
+            }
+            //Redirect back to Default.aspx
+            Response.Redirect(Url.Content("~"+PbiUtils.PbiReport.reportRedirecUrl),false);
+
+        }
+    }
+
+
+
+    
+}
---
+++ TSM/Utils/RestSharpHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+
+namespace TSM.Utils
+{
+    public class RestSharpHelper
+    {
+
+
+        public static IRestResponse GeneraPostRequest(string UrlBase, string Aplicacion, object Data)
+        {
+            string token = "";
+            if (System.Web.HttpContext.Current.Request.Cookies["t"] != null && !string.IsNullOrWhiteSpace(System.Web.HttpContext.Current.Request.Cookies.Get("t").Value))
+                token = System.Web.HttpContext.Current.Request.Cookies.Get("t").Value;
+
+
+            IRestResponse response = null;
+            var client = new RestClient(UrlBase);
+            var request = new RestRequest(Aplicacion,Method.POST);
+            request.RequestFormat = DataFormat.Json; //document type
+            request.AddHeader("t", token);
+            request.AddHeader("UserAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36");
+            
+            try
+            {
+                request.AddJsonBody(Data);
+                response = client.Execute(request);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return response;
+        }
+
+
+    }
+}
