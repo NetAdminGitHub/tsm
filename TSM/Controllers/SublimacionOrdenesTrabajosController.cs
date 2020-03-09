a69c2726d213027313d8d7127a30e093298a9c12
wlopez@techno-screen.com <wlopez@techno-screen.com>
2020-03-08 21:18:04 -0600

    se incluye vista de sublimacion

---
+++ TSM/Controllers/SublimacionOrdenesTrabajosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSM.Controllers
+{
+    public class SublimacionOrdenesTrabajosController : Controller
+    {
+        // GET: OrdenTrabajoSimulacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("SublimacionOrdenesTrabajos/SublimacionRegistro/{IdCliente}/{IdRequerimiento}")]
+        public ActionResult SublimacionOrdenesTrabajosReg(long IdCliente,long IdRequerimiento)
+        {
+            ViewData["IdCliente"] = IdCliente;
+            ViewData["IdRequerimiento"] = IdRequerimiento;
+            return View("SublimacionOrdenesTrabajosReg");
+        }
+    }
+}
