3dbac19cb4f7e476023b4fba34c426c5216850aa
wlopez@techno-screen.com <wlopez@techno-screen.com>
2020-07-21 00:27:26 -0600

    se incluye Bloqueo y notificación de etapas

--- TSM/Scripts/Hubs/Notificaciones.js
+++ TSM/Scripts/Hubs/Notificaciones.js
@@ -16,7 +16,16 @@
     };
 
     notif.client.actualizarVista = function (data) {
-        alert(data);
+        let d = JSON.parse(data);
+        if (d.Vista === loadedview && (loadedview !== undefined || loadedview !== "") && idOrdenTrabajo === d.Data.IdOrdenTrabajo && Number(idEtapaProceso) !== d.Data.IdEtapa && d.Data.IdUsuario !== getUser()) {
+            fn_AlertActualizaVista(d);
+        } 
+          //  else {
+        //    if (d.Vista === loadedview && (loadedview !== undefined || loadedview !== "") && idOrdenTrabajo === d.Data.IdOrdenTrabajo && Number(idEtapaProceso) !== d.Data.IdEtapa && d.Data.IdUsuario === getUser()) {
+        //        $("#" + d.Vista + "_action").trigger("Action");
+        //    }
+        //}
+        
     };
 
     $.connection.hub.url = TSM_Web_APi.replace("api/", "") + "signalr/hub";
@@ -26,3 +35,32 @@
     })
     .fail(function () { $("#kendoNotificaciones").data("kendoNotification").show('Imposible conectar a WebSocket!', 'error'); });
 });
+
+
+let fn_AlertActualizaVista = function (dt) {
+    $("#KendoAlerta").kendoAlert({
+        content: '<div class="form-row">' +
+            '<div class= "form-group col-lg-4 text-center" >' +
+            '<i class="k-icon k-i-warning" style="font-size:120px;"></i>' +
+            '</div >' +
+            '<div class="form-group col-lg-8">' +
+            '<h2>' + dt.Mensaje + '</h2>' +
+            '</div>' +
+            '</div >',
+        title: "warning!",
+        height: "auto",
+        width: "30%",
+        actions: [{
+            action: function (e) {
+                $("#" + dt.Vista + "_action").trigger("Action");
+                return true;
+            },
+            primary: true
+        }],
+        messages: {
+            okText: "OK"
+        }
+    }).data("kendoAlert");
+
+  
+};
