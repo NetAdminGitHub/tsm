7e43716c6d4826ad0103a7b2cc8551ce2baf5c0b
wlopez@techno-screen.com <wlopez@techno-screen.com>
2020-12-15 14:43:45 -0600

    se incluye vista mtto Tipo adjunto y mtto Motivo solicitudes de cambio

---
+++ TSM/Controllers/MotivosSolicitudesCambiosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSM.Controllers
+{
+    public class MotivosSolicitudesCambiosController : Controller
+    {
+        // GET: MotivosSolicitudesCambios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
