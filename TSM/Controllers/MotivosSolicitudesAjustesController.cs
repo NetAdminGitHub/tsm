545c47d9914eb5b81834e5e35e6f5f670b5377b7
Cesar Barrientos <cbarrientos@inquisalva.com>
2021-08-19 13:58:32 -0600

    Creación de CRUD de MotivosSolicitudesAjustes.

---
+++ TSM/Controllers/MotivosSolicitudesAjustesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSM.Controllers
+{
+    public class MotivosSolicitudesAjustesController : Controller
+    {
+        // GET: MotivosSolicitudesAjustes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
