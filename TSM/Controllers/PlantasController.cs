9ac24eef4e4b78c14ce80da6e3b18f6dbea10df1
Juan Menjivar <jmenjivar@inquisalva.com>
2021-10-29 09:59:42 -0600

    Se completó backend de planta #4061 #4055

---
+++ TSM/Controllers/PlantasController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TSM.BOL;
+
+namespace TSM.Controllers
+{
+    public class PlantasController : Controller
+    {
+        // GET: Maquinas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
