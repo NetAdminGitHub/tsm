5dfbcc088aab3c488c3c4a3e53cc6bc69fd64666
wlopez@techno-screen.com <wlopez@techno-screen.com>
2022-04-05 09:34:19 -0600

    se incluye vista de carrito

---
+++ TSM/Controllers/PreparacionCarritoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSM.Controllers
+{
+    public class PreparacionCarritoController : Controller
+    {
+        // GET: PreparacionCarrito
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
