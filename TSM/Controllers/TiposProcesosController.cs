6604535373b33fe71987fdd0553813be62841476
Cesar Barrientos <cbarrientos@inquisalva.com>
2022-05-23 17:06:25 -0600

    Vista de mantenedor de tipos de procesos (Producción o Desarrollo) #4735

---
+++ TSM/Controllers/TiposProcesosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSM.Controllers
+{
+    public class TiposProcesosController : Controller
+    {
+        // GET: TiposProcesos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
