55debc7377674240e832d3ed87460a043b9d469f
Diego Alejandro Rosa Iraheta <drosa@techno-screen.com>
2022-08-19 12:31:52 -0600

    Se anadió módulo de Macro y se modificó el módulo de micro.
    #4990

--- TSM/Scripts/js/EtapasProcesos.js
+++ TSM/Scripts/js/EtapasProcesos.js
@@ -78,6 +78,8 @@ $(document).ready(function () {
                     FechaMod: { type: "date" },
                     Estado: { type: "string" },
                     Nombre2: { type: "string" },
+                    Macro: { type: "string" },
+                    IdEtapaProcesoMacro: { type: "number" },
                     VistaFormulario: { type: "string" },
                     TablaEtapa: { type: "string" }
 
@@ -98,6 +100,12 @@ $(document).ready(function () {
             KdoHideCampoPopup(e.container, "FechaMod");
             KdoHideCampoPopup(e.container, "IconoView");
             $('[name="Icono"').attr('mayus', 'no');
+
+            $('[name="IdEtapaProcesoMacro"]').on('change', function (e) {
+                $("[name='Macro']").val($('[name="IdEtapaProcesoMacro"]').data("kendoComboBox").text());
+                $("[name='Macro']").trigger("change");
+            });
+
             Grid_Focus(e, "Nombre");
         },
 
@@ -119,6 +127,8 @@ $(document).ready(function () {
             { field: "TablaEtapa", title: "Tabla Etapa" },
             { field: "Nombre2", title: "Estado" },
             { field: "Estado", title: "Estado", values: ["Estado", "Nombre", UrlE, "EtapasProcesos", "Seleccione....", "required", "", "requerido"], editor: Grid_Combox, hidden: true },
+            { field: "Nombre3", title: "Macro Etapa" },
+            { field: "IdEtapaProcesoMacro", title: "Macro Etapa", editor: Grid_Combox, values: ["IdEtapaProcesoMacro", "Nombre", TSM_Web_APi + "EtapasProcesosMacro", "", "Seleccione....", "required", "", "requiredo"], hidden: true },
             {
                 template: "<div class='customer-photo' style='text-align:-webkit-center;'" +
                     "><span class='#: (data.Icono ===null? '': data.Icono).startsWith('k-i') === true ? 'k-icon ' + data.Icono : data.Icono  #' style='font-size:xx-large;'></span></div>",
---
+++ TSM/Scripts/js/MacroEtapas.js
@@ -0,0 +1,107 @@
+﻿var Permisos;
+let UrlPl = TSM_Web_APi + "EtapasProcesosMacro";
+
+$(document).ready(function () {
+    let dataSource = new kendo.data.DataSource({
+        //CONFIGURACION DEL CRUD
+        transport: {
+            read: {
+                url: UrlPl,
+                contentType: "application/json; charset=utf-8"
+            },
+            update: {
+                url: function (datos) { return UrlPl + "/" + datos.IdEtapaProcesoMacro; },
+                type: "PUT",
+                contentType: "application/json; charset=utf-8"
+            },
+            destroy: {
+                url: function (datos) { return UrlPl + "/" + datos.IdEtapaProcesoMacro; },
+                type: "DELETE"
+            },
+            create: {
+                url: UrlPl,
+                type: "POST",
+                contentType: "application/json; charset=utf-8"
+            },
+            parameterMap: function (data, type) {
+                if (type !== "read") {
+                    return kendo.stringify(data);
+                }
+            }
+        },
+        //Ordenando GRID
+
+        //FINALIZACIÓN DE UNA PETICIÓN
+        requestEnd: Grid_requestEnd,
+        // DEFINICIÓN DEL ESQUEMA, MODELO Y COLUMNAS
+        error: Grid_error,
+        schema: {
+            model: {
+                id: "IdEtapaProcesoMacro",
+                fields: {
+                    IdEtapaProcesoMacro: { type: "number", hidden:true },
+                    Nombre: {
+                        type: "string",
+                        validation: {
+                            //required: true,
+                            maxlength: function (input) {
+                                if (input.is("[name='Nombre']") && input.val().length === 0) {
+                                    input.attr("data-maxlength-msg", "Requerido");
+                                    return false;
+                                }
+                                if (input.is("[name='Nombre']") && input.val().length > 300) {
+                                    input.attr("data-maxlength-msg", "La longitud máxima del campo es 300");
+                                    return false;
+                                }
+                                return true;
+                            }
+                        }
+                    },
+                    Orden: { type: "number" },
+                    Icono: { type: "string" }
+                }
+            }
+        },
+        sort: { field: "IdEtapaProcesoMacro", dir: "asc" }
+    });
+
+    //CONFIGURACION DEL GRID,CAMPOS
+    $("#grid").kendoGrid({
+        edit: function (e) {
+            KdoHideCampoPopup(e.container, "IdEtapaProcesoMacro");
+            Grid_Focus(e, "Nombre");
+        },
+        //DEFINICIÓN DE LOS CAMPOS
+        columns: [
+            { field: "IdEtapaProcesoMacro", title: "ID", hidden: true },
+            { field: "Nombre", title: "Nombre" },
+            { field: "Orden", title: "Orden", editor: Grid_ColIntNumSinDecimal },
+            { field: "Icono", title: "Icono" }
+        ]
+    });
+
+    // FUNCIONES STANDAR PARA LA CONFIGURACION DEL GRID
+    SetGrid($("#grid").data("kendoGrid"), ModoEdicion.EnPopup, true, true, true, true, redimensionable.No);
+    SetGrid_CRUD_ToolbarTop($("#grid").data("kendoGrid"), Permisos.SNAgregar);
+    SetGrid_CRUD_Command($("#grid").data("kendoGrid"), Permisos.SNEditar, Permisos.SNBorrar);
+    Set_Grid_DataSource($("#grid").data("kendoGrid"), dataSource);
+
+    var selectedRows = [];
+    $("#grid").data("kendoGrid").bind("dataBound", function (e) { //foco en la fila
+        Grid_SetSelectRow($("#grid"), selectedRows);
+    });
+
+    $("#grid").data("kendoGrid").bind("change", function (e) {
+        Grid_SelectRow($("#grid"), selectedRows);
+    });
+    $(window).on("resize", function () {
+        Fn_Grid_Resize($("#grid"), $(window).height() - "371");
+    });
+
+    Fn_Grid_Resize($("#grid"), $(window).height() - "371");
+
+});
+
+fPermisos = function (datos) {
+    Permisos = datos;
+};
