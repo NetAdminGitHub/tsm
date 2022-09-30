85c5d18d6a4e7d1322a29a5bf09b50114aa3cdad
Diego Alejandro Rosa Iraheta <drosa@techno-screen.com>
2022-09-30 11:42:19 -0600

    Vista Código Modalidad y versionamiento de vistas Orden Despacho y Código Regímenes.
    #5162

---
+++ TSM/Scripts/js/CodigosModalidad.js
@@ -0,0 +1,123 @@
+﻿var Permisos;
+let UrlPl = TSM_Web_APi + "Modalidades";
+
+$(document).ready(function () {
+    let dataSource = new kendo.data.DataSource({
+        //CONFIGURACION DEL CRUD
+        transport: {
+            read: {
+                url: UrlPl,
+                contentType: "application/json; charset=utf-8"
+            },
+            update: {
+                url: function (datos) { return UrlPl + "/" + datos.IdModalidad; },
+                type: "PUT",
+                contentType: "application/json; charset=utf-8"
+            },
+            destroy: {
+                url: function (datos) { return UrlPl + "/" + datos.IdModalidad; },
+                type: "DELETE"
+            },
+            create: {
+                url: UrlPl,
+                type: "POST",
+                contentType: "application/json; charset=utf-8"
+            },
+            parameterMap: function (data, type) {
+                if (type !== "read") {
+                    return kendo.stringify(data);
+                }
+            }
+        },
+        //Ordenando GRID
+
+        //FINALIZACIÓN DE UNA PETICIÓN
+        requestEnd: Grid_requestEnd,
+        // DEFINICIÓN DEL ESQUEMA, MODELO Y COLUMNAS
+        error: Grid_error,
+        schema: {
+            model: {
+                id: "IdModalidad",
+                fields: {
+                    IdModalidad: { type: "number" },
+                    Nombre: {
+                        type: "string",
+                        validation: {
+                            //required: true,
+                            maxlength: function (input) {
+                                if (input.is("[name='Nombre']") && input.val().length === 0) {
+                                    input.attr("data-maxlength-msg", "Requerido");
+                                    return false;
+                                }
+                                if (input.is("[name='Nombre']") && input.val().length > 200) {
+                                    input.attr("data-maxlength-msg", "La longitud máxima del campo es 200");
+                                    return false;
+                                }
+                                if (input.is("[name='Codigo']") && input.val().length === 0) {
+                                    input.attr("data-maxlength-msg", "Requerido");
+                                    return false;
+                                }
+                                if (input.is("[name='Codigo']") && input.val().length > 2) {
+                                    input.attr("data-maxlength-msg", "La longitud máxima del campo es 2");
+                                    return false;
+                                }
+                                if (input.is("[name='Codigo']") && input.val() <0 ) {
+                                    input.attr("data-maxlength-msg", "El campo no adminte números negativos");
+                                    return false;
+                                }
+                                return true;
+                            }
+                        }
+                    },
+                    Codigo: { type: "string" },
+                    IdUsuarioMod: { type: "string" },
+                    FechaMod: { type: "date" }
+                }
+            }
+        },
+        sort: { field: "Codigo", dir: "asc" }
+    });
+
+    //CONFIGURACION DEL GRID,CAMPOS
+    $("#grid").kendoGrid({
+        edit: function (e) {
+            KdoHideCampoPopup(e.container, "IdModalidad");
+            KdoHideCampoPopup(e.container, "IdUsuarioMod");
+            KdoHideCampoPopup(e.container, "FechaMod");
+            Grid_Focus(e, "Nombre");
+        },
+        //DEFINICIÓN DE LOS CAMPOS
+        columns: [
+            { field: "IdModalidad", title: "ID", hidden: true },
+            { field: "Nombre", title: "Nombre" },
+            { field: "Codigo", title: "Código" },
+            { field: "IdUsuarioMod", title: "Usuario Mod", hidden: true },
+            { field: "FechaMod", title: "Fecha Mod", format: "{0: dd/MM/yyyy HH:mm:ss.ss}", hidden: true }
+        ]
+    });
+
+    // FUNCIONES STANDAR PARA LA CONFIGURACION DEL GRID
+    SetGrid($("#grid").data("kendoGrid"), ModoEdicion.EnPopup, true, true, true, true, redimensionable.No);
+    SetGrid_CRUD_ToolbarTop($("#grid").data("kendoGrid"), Permisos.SNAgregar);
+    SetGrid_CRUD_Command($("#grid").data("kendoGrid"), Permisos.SNEditar, Permisos.SNBorrar);
+    Set_Grid_DataSource($("#grid").data("kendoGrid"), dataSource);
+
+    var selectedRows = [];
+    $("#grid").data("kendoGrid").bind("dataBound", function (e) { //foco en la fila
+        Grid_SetSelectRow($("#grid"), selectedRows);
+    });
+
+    $("#grid").data("kendoGrid").bind("change", function (e) {
+        Grid_SelectRow($("#grid"), selectedRows);
+    });
+    $(window).on("resize", function () {
+        Fn_Grid_Resize($("#grid"), $(window).height() - "371");
+    });
+
+    Fn_Grid_Resize($("#grid"), $(window).height() - "371");
+
+});
+
+fPermisos = function (datos) {
+    Permisos = datos;
+};
