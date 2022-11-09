3fba5f4fc8d370bdda364c71a47ed3f11635f2bd
Diego Alejandro Rosa Iraheta <drosa@techno-screen.com>
2022-11-09 11:24:57 -0600

    Se añadió y configuró el stepper para el tracking de la OD.
    #5254

---
+++ TSM/Scripts/js/StepperOD.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    var etapas;
+    var objSteps = '$("#stepperOD").kendoStepper({linear: true,steps: [';
+
+    $.ajax({
+        dataType: 'json',
+        url: TSM_Web_APi + 'EtapasProcesos/GetbyTabla/Bodega¿DespachosMercancias',
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        success: function (result) {
+            etapas = result;
+            //console.log(etapas);
+            etapas.forEach(function (item, index, arr) {
+                objSteps = objSteps + '{label: "' + item.Nombre + '",icon: "' + item.Icono + '", successIcon: "check" },';
+            });
+
+            objSteps = objSteps.slice(0, -1);
+
+            objSteps = objSteps + '] }); ';
+
+            eval(objSteps);
+
+        }
+    });
+
+});
