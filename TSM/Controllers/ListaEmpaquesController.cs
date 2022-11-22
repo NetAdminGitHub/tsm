1d9816cef56b9872149d84a557f21dd7e39a9029
Diego Alejandro Rosa Iraheta <drosa@techno-screen.com>
2022-11-22 13:44:49 -0600

    Inicio creación PL.

---
+++ TSM/Controllers/ListaEmpaquesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSM.Controllers
+{
+    public class ListaEmpaquesController : Controller
+    {
+        // GET: ListaEmpaques
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("ListaEmpaques/{idOD}")]
+        public ActionResult Index(long idOD)
+        {
+            ViewBag.idOD = idOD;
+            return View();
+        }
+    }
+}
